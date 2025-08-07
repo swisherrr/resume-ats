03037c17b4e219d5518026927863c0aa192d34fe
swisher <zhswisher@gmail.com>
2025-08-07 14:51:57 -0400

    fixed NLTK

--- backend/services/resume_analyzer.py
+++ backend/services/resume_analyzer.py
@@ -18,6 +18,10 @@ try:
 except LookupError:
     nltk.download('punkt')
 try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab')
+try:
     nltk.data.find('corpora/stopwords')
 except LookupError:
     nltk.download('stopwords')
@@ -25,6 +29,10 @@ try:
     nltk.data.find('corpora/wordnet')
 except LookupError:
     nltk.download('wordnet')
+try:
+    nltk.data.find('tokenizers/averaged_perceptron_tagger')
+except LookupError:
+    nltk.download('averaged_perceptron_tagger')
 
 class ResumeAnalyzer:
     def __init__(self, db):
